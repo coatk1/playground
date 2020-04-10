9c3e090ae10200ce9bd2141f2f02b72fccc1a893
Corey Atkins <coatk1@umbc.edu>
2020-04-10 18:48:47 -0400

    added version

--- python/package/geo/__init__.py
+++ python/package/geo/__init__.py
@@ -3,3 +3,7 @@ from geo.calc import Distance
 from geo.geosp import Wt
 from geo.geosp import Gh
 from geo.files.csv_file import check
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
