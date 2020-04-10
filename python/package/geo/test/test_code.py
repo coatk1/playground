28778f80b58a50003390d1098daa60dae8294da8
Corey Atkins <coatk1@umbc.edu>
2020-04-10 18:39:32 -0400

    testing python 2.7

--- python/package/geo/test/test_code.py
+++ python/package/geo/test/test_code.py
@@ -1,13 +1,33 @@
+import sys
 from geo.calc import Calc, Distance
 
 
-def test_add():
-    assert Calc().add(2, 3) == 5, 'Should be 5'
+if sys.version_info[0] >= 3:
+
+    def test_add():
+        assert Calc().add(2, 3) == 5, 'Should be 5'
 
 
-def test_mul():
-    assert Calc().multiply(2, 3) == 6, 'Should be 6'
+    def test_mul():
+        assert Calc().multiply(2, 3) == 6, 'Should be 6'
 
 
-def test_power():
-    assert Distance(2).power(2) == 4, 'Should be 4'
+    def test_power():
+        assert Distance(2).power(2) == 4, 'Should be 4'
+
+else:
+    import unittest
+    
+    class TestCode(unittest.TestCase):
+
+        def test_add(self):
+            self.assertEqual(Calc().add(2, 3), 5, "Should be 5")
+
+        def test_mul(self):
+            self.assertEqual(Calc().multiply(2, 3), 6, "Should be 6")
+
+        def test_power(self):
+            self.assertEqual(Distance(2).power(2), 4, "Should be 4")
+
+    if __name__ == '__main__':
+        unittest.main()
