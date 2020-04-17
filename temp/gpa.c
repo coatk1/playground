c816b61fba6f02ce0ae7197cd810735fd3402b0b
Corey Atkins <coatk1@umbc.edu>
2020-04-17 18:06:01 -0400

    temp

---
+++ temp/gpa.c
@@ -0,0 +1,26 @@
+/* gpa.c
+** Corey Atkins
+** 06/13/13
+** coatk1@umbc.edu
+** This program will calculate the gpa of the user when prompt to enter their grade.
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	float userNum;
+	float credits;
+	int i;
+	char grade;
+
+	i = 0;
+
+	printf("Enter the number of credits you are taking this semester \n");
+	scanf("%f", credits);
+
+	printf("Enter the grade for your class (Between letters A and F): ");
+	scanf("%c", grade) 
+
+	return 0;
+}
