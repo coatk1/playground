2ea726af8dd963f0a20398550b795b23ee8ee75a
Corey Atkins <coatk1@umbc.edu>
2020-05-11 19:53:13 -0400

    blog

--- python/flask/flaskr/__init__.py
+++ python/flask/flaskr/__init__.py
@@ -10,6 +10,7 @@ import os
 from flask import Flask
 from . import db
 from . import auth
+from . import blog
 
 
 def create_app(test_config=None):
@@ -48,5 +49,11 @@ def create_app(test_config=None):
 
     # Register authentication
     app.register_blueprint(auth.bp)
+
+    # Register blog
+    app.register_blueprint(blog.bp)
+
+    # Blog will be the index of the app.
+    app.add_url_rule('/', endpoint='index')
 
     return app
