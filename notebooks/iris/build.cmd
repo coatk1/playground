d048cbeffa093537b64494bce1d1d88e73613276
Corey Atkins <coatk1@umbc.edu>
2020-08-11 20:40:18 -0400

    (#70)

---
+++ notebooks/iris/build.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+if exist C:\Users\Corey\Miniconda3\envs\pyBase1\Scripts\anaconda-project.exe %* goto yesfile
+if not exist C:\Users\Corey\Miniconda3\envs\pyBase1\Scripts\anaconda-project.exe %* goto nofile
+goto end
+:yesfile
+echo File does exist
+C:\Users\Corey\Miniconda3\envs\pyBase1\Scripts\anaconda-project.exe %* run
+goto end
+:nofile
+echo File does NOT exist
+conda create -n anaconda-project python=3 anaconda-project
+goto end
+:end
