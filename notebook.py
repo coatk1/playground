bf7f89a8482e360da76b5c4795078dbab171c330
Corey Atkins <coatk1@umbc.edu>
2021-08-06 11:22:10 -0400

    closes #99

---
+++ notebook.py
@@ -0,0 +1,31 @@
+```
+# Standard Libraries
+import os
+import csv
+import glob
+import pathlib
+# import getpass
+
+# Third-party Libraries
+import pandas
+# import ijson
+
+HOME_DRIVE = str(pathlib.Path.home())
+OTHER_DRIVE = "C:\\"
+
+# some_path = "path\\to\\file
+# path = os.path.join(HOME_DRIVE, some_path)
+
+# wp = getpass.getpass("Enter: ")
+
+# full_path = glob.glob("{}\\*.{}".format(path, "csv"))
+
+# pd.concat([pd.read_csv(_) for _ in full_path])
+
+# Read/ Write Files
+# Read in a file
+with open(file, "r") as f:
+
+
+
+```
