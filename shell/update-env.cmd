497a42603f357626b1e620447e36b0e3ea49fb68
Corey Atkins <coatk1@umbc.edu>
2021-08-06 13:31:25 -0400

    Update update-env.cmd

--- shell/update-env.cmd
+++ shell/update-env.cmd
@@ -6,23 +6,23 @@ REM Launches the windows cmd
 call PATH_TO_ANACONDA\activate.bat base
 
 REM Check if the environment exists.
-if exists %APPDATA%\conda\conda\env\dev\ %* goto yesfile
-if not exists %APPDATA%\conda\conda\env\dev\ %* goto nofile
+if exists %APPDATA%\conda\conda\env\my-dev\ %* goto yesfile
+if not exists %APPDATA%\conda\conda\env\my-dev\ %* goto nofile
 goto end
 
 :yesfile
 REM If it exists, update environment.
 echo Environment does exist, updating...
 pause
-call conda activate dev
-call conda env update coatk1/dev
+call conda activate my-dev
+call conda env update coatk1/my-dev
 goto end
 
 :nofile
 REM If it does not exists, install environment.
 echo Environment does not exist, installing...
 pause
-call conda env create coatk1/dev
+call conda env create coatk1/my-dev
 goto end
 
 :end
