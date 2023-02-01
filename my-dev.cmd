b5aebb365026406b7ff7dedc95d6a8d36e639f08
Corey Atkins <coatk1@umbc.edu>
2023-02-01 11:01:09 -0500

    Update and rename shell/update-env.cmd to my-dev.cmd

--- my-dev.cmd
+++ my-dev.cmd
@@ -12,17 +12,19 @@ goto end
 
 :yesfile
 REM If it exists, update environment.
-echo Environment does exist, updating...
+echo Environment does exist.
 pause
 call conda activate my-dev
-call conda env update coatk1/my-dev
+:: echo Updating environment
+:: call conda env update coatk1/my-dev
 goto end
 
 :nofile
 REM If it does not exists, install environment.
 echo Environment does not exist, installing...
 pause
-call conda env create coatk1/my-dev
+:: call conda env create coatk1/my-dev
+call conda env create -f dev.yml
 goto end
 
 :end
